ce1da7fa73784e00f1e25380a67bf3998939945c
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-10-12 01:55:07 +0300

    polymorphism updated

--- src/main/java/MythologyInterface/Main.java
+++ src/main/java/MythologyInterface/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Zeus zeus = new Zeus("Zeus", "Greek Mythology", "Father of god and men",true);
         Poseidon poseidon = new Poseidon("Poseidon", "Greek Mythology", "Creator of the seas",true);
         Hades hades = new Hades("Hades", "Greek Mythology", "Man of the underworld ",true);
-        Artemis artemis =new Artemis("Artemis","Grek","I'm a moon goddess",false);
-        Apollon apolllon = new Apollon("Apollon","Grek","I'm a sun god",false);
+        Zeus artemis =new Artemis("Artemis","Grek","I'm a moon goddess",false);
+        Zeus apolllon = new Apollon("Apollon","Grek","I'm a sun god",false);
         zeus.dispalyInfo();
         zeus.useSkySkil();
         artemis.dispalyInfo();
